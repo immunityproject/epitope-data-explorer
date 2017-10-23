374d2d56acdeea34d8de9840c889ae6b8631d457
Rick Carback <carback1@lexumo.com>
2017-10-23 04:46:17 +0000

    Initial code to query and render Epitope data

--- src/Epitopes.js
+++ src/Epitopes.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import axios from 'axios';
 import { Link, Switch, Route } from 'react-router-dom'
 
 
@@ -6,8 +7,15 @@ import { Link, Switch, Route } from 'react-router-dom'
 //       populate the epitope API.
 const EpitopeAPI = {
   proteins: [
-    { number: 1, name: "P17" },
-    { number: 2, name: "P24" }
+    { number: 1, name: "GP120", data: null },
+    { number: 2, name: "INT", data: null },
+    { number: 3, name: "NEF", data: null },
+    { number: 4, name: "P17", data: null },
+    { number: 5, name: "P24", data: null },
+    { number: 6, name: "PRO", data: null },
+    { number: 7, name: "REV", data: null },
+    { number: 8, name: "RT", data: null },
+    { number: 9, name: "TAT", data: null }
   ],
   all: function() { return this.proteins},
   get: function(id) {
@@ -34,19 +42,59 @@ const EpitopeListing = () => (
 // The Epitope looks up the protein using the number parsed from
 // the URL's pathname. If no protein is found with the given
 // number, then a "protein not found" message is displayed.
-const Epitope = (props) => {
-  const Epitope = EpitopeAPI.get(
-    parseInt(props.match.params.number, 10)
-  )
-  if (!Epitope) {
-    return <div>Protein was not found</div>
+class Epitope extends React.Component{
+  constructor(props) {
+    super(props);
+    const epitope = EpitopeAPI.get(parseInt(props.match.params.number, 10));
+    if (epitope) {
+      this.state = {
+        number: epitope.number,
+        name: epitope.name,
+        data: null
+      };
+    } else {
+      this.state = {
+        number: null,
+        name: null,
+        data: null
+      };
+    }
+
+    // Now load protein data using axios promise interface
+    // TODO: Make the base path to the data configurable
+    axios.get('/' + this.state.name).then(res => {
+      this.setState({
+        number: this.state.number,
+        name: this.state.name,
+        data: res.data
+      });
+    }).catch(function (error) {
+      console.log(error);
+      this.setState({
+        number: this.state.number,
+        name: this.state.name,
+        data: error
+      });
+    });
   }
-  return (
-    <div>
-      <h1>{Epitope.name} (#{Epitope.number})</h1>
-      <Link to='/epitopes'>Back</Link>
-    </div>
-  )
+
+  render() {
+    if (this.state.number == null) {
+      return (<div>Protein was not found</div>);
+    }
+    if (this.state.data == null) {
+      return (<div>Protein data loading...</div>);
+    }
+
+    let datastr = JSON.stringify(this.state.data);
+    return (
+        <div>
+          <h1>{this.state.name} (#{this.state.number})</h1>
+          <p>{datastr}</p>
+          <Link to='/epitopes'>Back</Link>
+        </div>
+    );
+  }
 }
 
 // The Listing component matches one of two different routes
