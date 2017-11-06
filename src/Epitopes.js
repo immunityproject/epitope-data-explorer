331e625af37b178a1c530b06a4df61adf283c62e
Rick Carback <carback1@lexumo.com>
2017-11-06 08:09:05 +0000

    Update Epitopes display to enable cycling through sites and showing a histogram

--- src/Epitopes.js
+++ src/Epitopes.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import axios from 'axios';
-import { Link, Switch, Route } from 'react-router-dom'
-
+import { Switch, Route } from 'react-router-dom';
+import { Chart } from 'react-google-charts';
 
 // TODO: Load a configuration file with this information to
 //       populate the epitope API.
@@ -24,85 +24,205 @@ const EpitopeAPI = {
   }
 }
 
-// EpitopeListing generates a link for each epitope
-const EpitopeListing = () => (
-  <div>
-    <ul>
-      {
-        EpitopeAPI.all().map(e => (
-          <li key={e.number}>
-            <Link to={`/epitopes/${e.number}`}>{e.name}</Link>
-          </li>
-        ))
-      }
-    </ul>
-  </div>
-)
+class EpitopeList extends React.Component {
+  render() {
+    return (
+        <select value={this.props.value} onChange={this.props.onChange}>
+        <option value='0'>Select an Epitope</option>
+        {
+          EpitopeAPI.all().map(e => (
+              <option key={e.number} value={e.number}>
+                {e.name}
+              </option>
+          ))
+        }
+        </select>
+    )
+  }
+}
 
-// The Epitope looks up the protein using the number parsed from
-// the URL's pathname. If no protein is found with the given
-// number, then a "protein not found" message is displayed.
-class Epitope extends React.Component{
+class MutationList extends React.Component {
+  render() {
+    return (
+        <select value={this.props.value} onChange={this.props.onChange}>
+        {
+          this.props.mutations.map((m, i) => (
+              <option key={i} value={m}>
+                {m}
+              </option>
+          ))
+        }
+        </select>
+    )
+  }
+}
+
+class SiteList extends React.Component {
+  render() {
+    return (
+        <select value={this.props.value} onChange={this.props.onChange}>
+        {
+          this.props.sites.map((m, i) => (
+              <option key={i} value={m}>
+                {m}
+              </option>
+          ))
+        }
+        </select>
+    )
+  }
+}
+
+// EpitopeHistogram generates a link for each epitope
+class EpitopeHistogram extends React.Component {
+
   constructor(props) {
     super(props);
-    const epitope = EpitopeAPI.get(parseInt(props.match.params.number, 10));
-    if (epitope) {
-      this.state = {
-        number: epitope.number,
-        name: epitope.name,
-        data: null
-      };
-    } else {
-      this.state = {
-        number: null,
-        name: null,
-        data: null
-      };
-    }
+    this.state = {
+      number: 0,
+      name: '',
+      data: null,
+      mutations: [],
+      selected_mutation: 0,
+      sites: [],
+      startsite: 0,
+      endsite: 0
+    };
 
     // Now load protein data using axios promise interface
     // TODO: Make the base path to the data configurable
-    axios.get('/' + this.state.name).then(res => {
+    axios.get('/P17').then(res => {
       this.setState({
-        number: this.state.number,
-        name: this.state.name,
         data: res.data
       });
-    }).catch(function (error) {
+    }).catch((error) => {
       console.log(error);
-      this.setState({
-        number: this.state.number,
-        name: this.state.name,
-        data: error
-      });
     });
   }
 
-  render() {
-    if (this.state.number == null) {
-      return (<div>Protein was not found</div>);
+  changeEpitope = (event) => {
+    if (event.target.value === '0') {
+      return
     }
-    if (this.state.data == null) {
-      return (<div>Protein data loading...</div>);
-    }
+    // The target Epitope name/id
+    const epitope = EpitopeAPI.get(parseInt(event.target.value, 10));
+    // Shortcut to the epitope data we care about
+    const edata = this.state.data[epitope.name]
+    // List of valid mutations
+    const mutations = (Object.keys(this.state.data[epitope.name]).filter(
+      mutation =>
+        edata[mutation]['energies'][epitope.name] != null)).sort();
+    // List of sites inside these valid mutations
+    const sites = (Object.keys(
+      edata[mutations[0]]['energies'][epitope.name])).sort((a, b) => a - b)
 
-    let datastr = JSON.stringify(this.state.data);
-    return (
+    this.setState({
+      number: epitope.number,
+      name: epitope.name,
+      mutations: mutations,
+      selected_mutation: mutations[0],
+      sites: sites,
+      data: this.state.data,
+      startsite: sites[0],
+      endsite: sites[0]
+    });
+  }
+
+  changeMutation = (event) => {
+    const epitope = this.state.name;
+    const mutation = event.target.value;
+    const mdata = this.state.data[epitope][mutation];
+    const sites = (Object.keys(
+      mdata['energies'][epitope])).sort((a, b) => a - b);
+    this.setState({
+      selected_mutation: mutation,
+      sites: sites,
+      startsite: sites[0],
+      endsite: sites[0]
+    })
+  }
+
+  changeStartSite = (event) => {
+    const start = event.target.value;
+    const end = Math.max(start, this.state.endsite);
+    this.setState({
+      startsite: start,
+      endsite: end
+    })
+  }
+
+  changeEndSite = (event) => {
+    const end = event.target.value;
+    const start = Math.min(end, this.state.startsite);
+    this.setState({
+      startsite: start,
+      endsite: end
+    })
+  }
+
+  render = () => {
+    if (this.state.number === 0) {
+      return (
         <div>
-          <h1>{this.state.name} (#{this.state.number})</h1>
-          <Link to='/epitopes'>Go Back</Link>
-          <p>{datastr}</p>
+        <EpitopeList value={this.state.number} onChange={this.changeEpitope} />
         </div>
-    );
+      )
+    }
+    const epitope = this.state.name;
+    const mutation = this.state.selected_mutation;
+    const start = this.state.startsite;
+    const end = this.state.endsite;
+    const site_data = this.state.data[epitope][mutation]['energies'][epitope];
+    const sites = Object.keys(site_data).filter(
+        x => x >= start && x <= end);
+    let seriesdata = [];
+    sites.forEach(function (site) {
+      site_data[site].forEach(function(x) {
+        seriesdata.push([ x.mutation + '/' + x.wt, parseFloat(x.energyDelta)])
+      })
+    });
+    const chartdata = [['Mut/WT', 'Energy Delta']].concat(seriesdata);
+
+    return (
+        <div style={{fontFamily:'sans-serif',fontSize:'0.8em'}}>
+        <EpitopeList value={this.state.number} onChange={this.changeEpitope} />
+        <MutationList value={this.state.selected_mutation}
+          mutations={this.state.mutations} onChange={this.changeMutation} />
+        <SiteList value={this.state.startsite} onChange={this.changeStartSite}
+          sites={this.state.sites}/>
+        <SiteList value={this.state.endsite} onChange={this.changeEndSite}
+          sites={this.state.sites}/>
+        <br /><br />
+        <Chart
+          chartType="ColumnChart"
+          data={chartdata}
+          options={{}}
+          graph_id="ColumnChart"
+          width="100%"
+          height="600px"
+          legend_toggle
+        />
+        </div>
+    )
   }
 }
+
+        // <Chart
+        //   chartType="ColumnChart"
+        //   data={chartdata}
+        //   options={{}}
+        //   graph_id="ColumnChart"
+        //   width="100%"
+        //   height="400px"
+        //   legend_toggle
+        // />
+
 
 // The Listing component matches one of two different routes
 // depending on the full pathname
 const Epitopes = () => (
   <Switch>
-    <Route exact path='/epitopes' component={EpitopeListing}/>
-    <Route path='/epitopes/:number' component={Epitope}/>
+    <Route exact path='/epitopes' component={EpitopeHistogram}/>
   </Switch>
 )
 
